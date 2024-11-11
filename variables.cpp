4a45777d12982866a94a2c42eddacb025cd637e1
Rohan Karappath <rohankarappath@gmail.com>
2024-11-11 10:11:01 +0530

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter two numbers:");
+	scanf("%d %d",&a,&b);
+	int a=15;
+	int b=7;
+	int difference=0
+	{
+		while(a<b)
+		difference=a-b;
+		printf("current difference is %d\n",difference);
+		break;
+	}
+	printf("%d is final differnce\n",difference);
+	return 0;
+}
