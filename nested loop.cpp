ccfe96d258c2b62e564487b7b032cdd1a8acf351
Rohan Karappath <rohankarappath@gmail.com>
2024-11-11 10:04:16 +0530

    Add files via upload

---
+++ nested loop.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,k;
+{
+	for(int i=1; i<=3; i++)
+	{
+		for (int j=1; j<=3; j++)
+		{
+			for (int k=1; k<=3; k++)
+			printf("i=%d j=%d k=%d\n" ,i,j,k);
+		}
+    }
+}
+return 0;
+}
---
+++ while+rev.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num=1234, rev=0;
+	while(num !=0)
+	{
+		rev=rev*10+num%10;
+		num/=10;
+	}
+	printf("Reversed Number=%d\n", rev);
+	return 0;
+}
